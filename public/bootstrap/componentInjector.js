367a39ce67b8173a11522ffc737e41a9d094c318
drelocatelli <andressalocatelli@hotmail.com>
2023-04-11 10:52:00 -0300

    fix undefined component children

--- public/bootstrap/componentInjector.js
+++ public/bootstrap/componentInjector.js
@@ -2,10 +2,11 @@ const components = document.querySelectorAll('component');
 
 async function insertComponents() {
     for(let component of components) {
-        const src = component.getAttribute('src').concat('.html');
+        console.log(component)
+        const src = component.getAttribute('src');
         let response = await fetch(src);
         let componentHTML = (response.status == 404) ? '[component not found]' : await response.text();
-        componentHTML = componentHTML.replace(/<Slot \/>/g, component.children[0].outerHTML)
+        componentHTML = componentHTML.replace(/<Slot \/>/g, component.children[0]?.outerHTML)
         component.outerHTML = componentHTML;
     }
 }
